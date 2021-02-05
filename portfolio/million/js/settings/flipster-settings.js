011ca8f19cc4e4b8a10fe49232c02ccfd0969345
Architector404 <architector404@gmail.com>
2021-02-05 13:20:52 +0700

    Add files via upload

---
+++ portfolio/million/js/settings/flipster-settings.js
@@ -0,0 +1,9 @@
+$("#carousel").flipster({
+    style: 'carousel',
+    spacing: 0,
+    nav: false,
+    buttons: 'custom',
+    loop: true,
+    buttonPrev: '<img width="70" src="img/icons/nav1.png">',
+    buttonNext: '<img width="70" src="img/icons/nav2.png">'
+});
